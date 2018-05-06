ee826e8c244641c721361a89b6b7a91259ca9dc7
Kent <khmakishima@ucdavis.edu>
2018-05-06 07:56:47 -0700

    connected backend to satori

--- zeraHealth/js/controllers.js
+++ zeraHealth/js/controllers.js
@@ -21,6 +21,37 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+    $scope.myFunc=function(){
+        var endpoint = '############';
+        var appkey = '############';
+
+        var client = new RTM(endpoint, appkey);
+
+        client.on('enter-connected', function () {
+          console.log('Connected to Satori RTM!');
+
+          var channelName = 'emaildoc';
+          var message = {
+            who: '%s'%inputx,
+          };
+          client.publish(channelName, message , function (pdu) {
+            if (pdu.action === 'rtm/publish/ok') {
+              console.log('Publish confirmed');
+            } else {
+              console.log('Failed to publish. RTM replied with the error ' +
+                  pdu.body.error + ': ' + pdu.body.reason);
+            }
+          });
+        });
+
+        client.on('error', function (error) {
+          console.log('Failed to connect', error);
+        });
+
+        client.start();
+    }
+            
+    };
 
 }])
       
