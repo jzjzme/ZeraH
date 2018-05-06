30aa5a6316f33a956a423ed764f6ee9b6e86eba7
Kent <khmakishima@ucdavis.edu>
2018-05-05 18:49:38 -0700

    added redox

---
+++ backend/js/redox.js
@@ -0,0 +1,353 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var path = require('path');
+var _ = require('lodash');
+
+var DESTINATION_VERIFICATION_TOKEN = process.env.DESTINATION_VERIFICATION_TOKEN;
+var SOURCE_API_KEY = process.env.SOURCE_API_KEY;
+var SOURCE_SECRET = process.env.SOURCE_SECRET;
+var authToken, authTokenExpires;
+
+var lowdb = require('lowdb');
+var db = lowdb('db.json');
+
+db.defaults({ 
+	appointments: [{
+		"Meta": {
+			"DataModel": "Scheduling",
+			"EventType": "New",
+			"EventDateTime": "2018-01-09T05:35:18.024Z",
+			"Test": true,
+			"Destinations": [
+				{
+					"ID": "af394f14-b34a-464f-8d24-895f370af4c9",
+					"Name": "Redox EMR"
+				}
+			],
+			"FacilityCode": null
+		},
+		"Patient": {
+			"Identifiers": [
+				{
+					"ID": "0000000002",
+					"IDType": "MR"
+				},
+				{
+					"ID": "81ac9647-3a07-46b7-85b4-c5d09099fb3b",
+					"IDType": "EHRID"
+				},
+				{
+					"ID": "ffc486eff2b04b8^^^&1.3.6.1.4.1.21367.2005.13.20.1000&ISO",
+					"IDType": "NIST"
+				}
+			],
+			"Demographics": {
+				"FirstName": "Jackie",
+				"MiddleName": null,
+				"LastName": "Zhang",
+				"DOB": "1995-03-24",
+				"SSN": "202-02-0002",
+				"Sex": "Make",
+				"Race": "Asian",
+				"IsHispanic": null,
+				"MaritalStatus": "Single",
+				"IsDeceased": null,
+				"DeathDateTime": null,
+				"PhoneNumber": {
+					"Home": "+16269329999",
+					"Office": "+16269329999",
+					"Mobile": "+16269329999"
+				},
+				"EmailAddresses": [
+					"jackie@jackie.com"
+				],
+				"Language": "en",
+				"Citizenship": [],
+				"Address": {
+					"StreetAddress": "1111 SF Street",
+					"City": "SF",
+					"State": "CA",
+					"ZIP": "94661",
+					"County": "SF",
+					"Country": "US"
+				}
+			},
+			"Notes": []
+		},
+		"AppointmentInfo": [
+			{
+				"Code": "23457",
+				"Codeset": "Redox EHR Codes",
+				"Description": "Priority",
+				"Value": "Normal"
+			},
+			{
+				"Code": "23457",
+				"Codeset": "Redox EHR Codes",
+				"Description": "Form",
+				"Value": "Lumbar"
+			}
+		],
+		"Visit": {
+			"VisitNumber": "1234",
+			"AccountNumber": null,
+			"VisitDateTime": "2018-01-10T03:40:16.525Z",
+			"PatientClass": "Outpatient",
+			"Status": null,
+			"Duration": 15,
+			"Reason": "Check up",
+			"Instructions": [],
+			"AttendingProvider": {
+				"ID": "4236464757",
+				"IDType": "NPI",
+				"FirstName": "Kent",
+				"LastName": "Makishima",
+				"Credentials": [
+					"MD"
+				],
+				"Address": {
+					"StreetAddress": "500 Geary",
+					"City": "SF",
+					"State": "CA",
+					"ZIP": "94661",
+					"County": "SF",
+					"Country": "USA"
+				},
+				"Location": {
+					"Type": null,
+					"Facility": null,
+					"Department": null,
+					"Room": null
+				},
+				"PhoneNumber": {
+					"Office": "+13145554321"
+				}
+			},
+			"ConsultingProvider": {
+				"ID": null,
+				"IDType": null,
+				"FirstName": null,
+				"LastName": null,
+				"Credentials": [],
+				"Address": {
+					"StreetAddress": null,
+					"City": null,
+					"State": null,
+					"ZIP": null,
+					"County": null,
+					"Country": null
+				},
+				"Location": {
+					"Type": null,
+					"Facility": null,
+					"Department": null,
+					"Room": null
+				},
+				"PhoneNumber": {
+					"Office": null
+				}
+			},
+			"ReferringProvider": {
+				"ID": "4356789876",
+				"IDType": "NPI",
+				"FirstName": "Pat",
+				"LastName": "Granite",
+				"Credentials": [
+					"MD"
+				],
+				"Address": {
+					"StreetAddress": "123 Main St.",
+					"City": "Madison",
+					"State": "WI",
+					"ZIP": "53703",
+					"County": "Dane",
+					"Country": "USA"
+				},
+				"Location": {
+					"Type": null,
+					"Facility": null,
+					"Department": null,
+					"Room": null
+				},
+				"PhoneNumber": {
+					"Office": "+16085551234"
+				}
+			},
+			"VisitProvider": {
+				"ID": null,
+				"IDType": null,
+				"FirstName": null,
+				"LastName": null,
+				"Credentials": [],
+				"Address": {
+					"StreetAddress": null,
+					"City": null,
+					"State": null,
+					"ZIP": null,
+					"County": null,
+					"Country": null
+				},
+				"Location": {
+					"Type": null,
+					"Facility": null,
+					"Department": null,
+					"Room": null
+				},
+				"PhoneNumber": {
+					"Office": null
+				}
+			},
+			"Location": {
+				"Type": "Inpatient",
+				"Facility": "RES General Hospital",
+				"Department": "3S",
+				"Room": "136"
+			},
+			"Diagnoses": [
+				{
+					"Code": "N39.0",
+					"Codeset": "ICD-10",
+					"Name": "Some stuff, site not specified",
+					"Type": null
+				},
+				{
+					"Code": "L20.84",
+					"Codeset": "ICD-10",
+					"Name": "Intrinsic (allergic) eczema",
+					"Type": null
+				}
+			]
+		}
+	}]
+}).write();
+
+db._.mixin({ last: (array) => array[array.length - 1] });
+
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3000, function () {
+	console.log('Server started. Listening on port 3000.');
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.get('/api/scheduling', (req, res) => {
+	const appointments = db.get('appointments').last().value();
+	res.send(appointments);
+});
+
+app.post('/api/visitupdate', (req, res) => {
+	console.log(req.body);
+	console.log(req.body);
+
+	const Patient = _.get(req, 'body.Patient');
+	const Visit = _.get(req, 'body.Visit');
+	Visit['PatientClass'] = Visit['PatientClass'] || 'Outpatient';  //lol
+
+	getAuthToken(toke => {
+		var options = {
+			url: 'https://api.redoxengine.com/endpoint',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + toke
+			},
+			json: true,
+			body: {
+				Meta: {
+					DataModel: "PatientAdmin",
+					EventType: "VisitUpdate",
+					EventDateTime: new Date().toISOString(),
+					Test: true,
+				},
+				Patient,
+				Visit
+			}
+		};
+
+		request.post(options, function (err, response, body) {
+			if (err) {
+				console.log(err);
+				res.status(500).send(err);
+			}
+
+			console.log(response.statusCode);
+			console.log(body.Meta.DataModel + " was received");
+			res.send('neat');
+		});
+	});
+});
+
+app.get('/api', function (req, res) {
+	if (req.headers['verification-token'] === DESTINATION_VERIFICATION_TOKEN) {
+		console.log('verification-token matched!');
+		return res.send(req.query.challenge);
+	}
+
+	console.log('verfication-token did not match :( ');
+	res.sendStatus(400);
+});
+
+app.post('/api', function (req, res) {
+
+	if (req.headers['verification-token'] !== DESTINATION_VERIFICATION_TOKEN) {
+		console.log('Bad verification token.');
+		return res.sendStatus(403);
+	}
+
+	if (!_.get(req, 'body.Meta.DataModel') === 'Scheduling' ||
+		!_.get(req, 'body.Meta.EventType') === 'New') {
+		res.sendStatus(400);
+	}
+
+	console.log('Patient Registration Received');
+
+	db.get('appointments')
+		.push(_.get(req, 'body'))
+		.write();
+
+	const patientEmail = _.get(req, 'body.Patient.Demographics.EmailAddresses[0]');
+	const patientPhone = _.get(req, 'body.Patient.Demographics.PhoneNumber.Mobile');
+	console.log(`Texting patient at ${patientPhone} and emailing patient at ${patientEmail}`);
+
+	res.sendStatus(200);
+});
+
+app.get('/appointments', function (req, res) {
+	var appointments = db.get('appointments').value();
+	res.send(appointments);
+});
+
+function getAuthToken(callback) {
+	if (authToken && Date.now() < new Date(authTokenExpires).getTime()) {
+		return callback(authToken);
+	} else {
+		//get new token
+
+		var options = {
+			url: 'https://api.redoxengine.com/auth/authenticate',
+			method: 'POST',
+			body: {
+				apiKey: SOURCE_API_KEY,
+				secret: SOURCE_SECRET
+			},
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			json: true
+		};
+
+		request.post(options, function (err, response, body) {
+			console.log(body);
+
+			authToken = body.accessToken;
+			authTokenExpires = body.expires;
+
+			callback(authToken);
+		});
+	}
+}
---
+++ backend/python/server.py
@@ -0,0 +1,26 @@
+#server.py
+
+#da real backend
+
+
+import dbserver.py
+
+global p_name
+global patientdata
+
+def pullpatientdata(p_name):
+	#this is where patient data is taken from. 
+	patientdata = getparts(p_name)
+
+	return patientdata
+
+
+def risks(p_name):
+	patient = pullpatientdata(p_name)
+
+
+def data_create(url):
+	#fill in the ml.
+
+
+
